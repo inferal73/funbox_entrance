848c40433752ed3b2c5dadd47e53e0b337bc9755
Ivan <inferal73@gmail.com>
2018-01-28 16:25:04 +0400

    обработчик для ховера на выбраную карточку

--- src/main.js
+++ src/main.js
@@ -3,27 +3,37 @@ require('./scss/main.scss');
 /* END REQUIRE STYLE */
 
 const app = {
-  data: {
-    cards: [],
+  setCardState(input) {
+    const card = input.el;
+    if (input.checked) {
+      card.classList.add('card-selected');
+    } else {
+      card.classList.remove('card-selected');
+    }
   },
-  methods: {
-    setCardState(input) {
-      const card = input.el;
-      if (input.checked) {
-        card.classList.add('card-selected');
-      } else {
-        card.classList.remove('card-selected');
-      }
-    },
-    setCardToChecked(event) {
-      event.preventDefault();
-      const cardInput = event.currentTarget.input;
-      cardInput.checked = true;
-      this.setCardState(cardInput);
-    },
+  setCardToChecked(event) {
+    event.preventDefault();
+    const cardInput = event.currentTarget.input;
+    cardInput.checked = true;
+    this.setCardState(cardInput);
+  },
+  selectedHover() {
+    const cards = [...document.querySelectorAll('.card')];
+    cards.forEach((card) => {
+      card.addEventListener('mouseenter', (event) => {
+        const currentCard = event.target;
+        if (currentCard.classList.contains('card-selected')) {
+          currentCard.classList.add('card-selected-hover-style');
+        }
+      });
+      card.addEventListener('mouseleave', (event) => {
+        const currentCard = event.target;
+        currentCard.classList.remove('card-selected-hover-style');
+      });
+    });
   },
   init() {
-    this.data.cards = [...document.querySelectorAll('.card')].forEach((card) => {
+    [...document.querySelectorAll('.card')].forEach((card) => {
       const cardInput = card.querySelector('input');
 
       const buyLink = card.querySelector('.js-buy');
@@ -32,9 +42,10 @@ const app = {
 
       card.input = cardInput;
       cardInput.el = card;
-      cardInput.addEventListener('change', event => this.methods.setCardState(event.currentTarget));
-      buyLink.addEventListener('click', event => this.methods.setCardToChecked(event));
+      cardInput.addEventListener('change', event => this.setCardState(event.currentTarget));
+      buyLink.addEventListener('click', event => this.setCardToChecked(event));
     });
+    this.selectedHover();
   },
 };
 
